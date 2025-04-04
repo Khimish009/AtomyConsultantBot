4eb8c0aae6b7ae246a6d57596883fad5dd3c7738
Сальман Химишев <salman.khimishev01@gmail.com>
2025-04-04 16:28:41 +0300

    feat(*): initialization

---
+++ go.mod
@@ -0,0 +1,5 @@
+module atomy-consultant-bot
+
+go 1.24.1
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+}
+
+func main() {
+	token := mustToken()
+	fmt.Printf("Work", token)
+}
+
+func mustToken() string {
+	token := os.Getenv("TG_BOT_TOKEN")
+
+	if token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return token
+}
